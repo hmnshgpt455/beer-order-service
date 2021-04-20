118b57eaf5d77be3ee63ad9d427ea8b8cc30d008
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-20 11:13:18 +0530

    Adding awaitility. Closes #31.

--- src/test/java/io/github/hmnshgpt455/beerorderservice/services/BeerOrderManagerImplIT.java
+++ src/test/java/io/github/hmnshgpt455/beerorderservice/services/BeerOrderManagerImplIT.java
@@ -28,6 +28,7 @@ import static com.github.jenspiegsa.wiremockextension.ManagedWireMockServer.with
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -82,10 +83,15 @@ public class BeerOrderManagerImplIT {
         BeerOrder beerOrder = createBeerOrder();
 
         BeerOrder savedBeerOrder = beerOrderManager.newBeerOrder(beerOrder);
-        Thread.sleep(4000);
-        BeerOrder savedBeerOrder2 = beerOrderRepository.findById(savedBeerOrder.getId()).get();
+        await().untilAsserted(() -> {
+            BeerOrder foundOrder = beerOrderRepository.findById(beerOrder.getId()).get();
+
+            //todo - ALLOCATED STATUS
+            assertEquals(BeerOrderStatusEnum.PENDING_INVENTORY_ALLOCATION, foundOrder.getOrderStatus());
+        });
+        savedBeerOrder = beerOrderRepository.findById(savedBeerOrder.getId()).get();
         assertNotNull(savedBeerOrder);
-        assertEquals(BeerOrderStatusEnum.INVENTORY_ALLOCATED, savedBeerOrder2.getOrderStatus());
+        assertEquals(BeerOrderStatusEnum.INVENTORY_ALLOCATED, savedBeerOrder.getOrderStatus());
 
     }
 
